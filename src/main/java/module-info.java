549a7952c398be067a32266a66c52309a9e064d6
Albaraa Alsayed <albaraaalsayed4@gmail.com>
2025-12-29 14:35:37 +0200

    Initial XO logic

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,5 +3,7 @@ module com.mycompany.tic_tac_toe_app {
     requires javafx.fxml;
 
     opens com.mycompany.tic_tac_toe_app to javafx.fxml;
+    opens com.mycompany.tic_tac_toe_app.controls to javafx.fxml;
+
     exports com.mycompany.tic_tac_toe_app;
 }
