32763fa5e36830efcf487568aa8799a7890e9646
Thaowpsta <thaowpstasaiid@gmail.com>
2025-12-31 14:52:42 +0200

    made Register, Saved Games screens

---
+++ src/main/java/com/mycompany/tic_tac_toe_app/controllers/RegisterController.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.mycompany.tic_tac_toe_app.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+/**
+ * FXML Controller class
+ *
+ * @author thaowpstasaiid
+ */
+public class RegisterController implements Initializable {
+
+    @FXML
+    private TextField username;
+    @FXML
+    private TextField password;
+    @FXML
+    private TextField confirmPassowrd;
+    @FXML
+    private Button signUpBtn;
+    @FXML
+    private Text signIn;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void signUp(ActionEvent event) {
+    }
+
+    @FXML
+    private void NavigateToSignIn(MouseEvent event) {
+    }
+    
+}
