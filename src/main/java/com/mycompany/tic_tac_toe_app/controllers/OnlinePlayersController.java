91ec2f12a12edec0c11c95885140561c918d5c86
AbdullhGaber <abdullh760@yahoo.com>
2026-01-07 22:07:40 +0200

    ~ Refactoring OnlinePlayersController.java so it can show actual online players from server not dummy data

--- src/main/java/com/mycompany/tic_tac_toe_app/controllers/OnlinePlayersController.java
+++ src/main/java/com/mycompany/tic_tac_toe_app/controllers/OnlinePlayersController.java
@@ -1,14 +1,9 @@
 package com.mycompany.tic_tac_toe_app.controllers;
 
-import com.mycompany.tic_tac_toe_app.App;
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
-import javafx.beans.InvalidationListener;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+import com.mycompany.tic_tac_toe_app.model.PlayerDTO;
+import com.mycompany.tic_tac_toe_app.network.Client;
+import com.mycompany.tic_tac_toe_app.network.ClientProtocol;
+import com.mycompany.tic_tac_toe_app.util.Functions;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -18,88 +13,53 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.Set;
+import javafx.scene.layout.Priority;
 
 public class OnlinePlayersController implements Initializable {
 
     @FXML
-    private Pane onlinePlayersPane;
+    private ListView<HBox> playersListView;
     @FXML
-    private HBox onlineAppBar;
+    private Label noPlayersLabel;
     @FXML
     private TextField searchBar;
 
-    @FXML
-    private ListView<HBox> playersListView;
-
-    private List<Player> allPlayers = new ArrayList<>();
-    @FXML
-    private Label noPlayersLabel;
+    private Set<PlayerDTO> onlinePlayers;
+    private Client client;
+    private ClientProtocol cp;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        allPlayers.add(new Player("Ahmed", "Online", "Invite"));
-        allPlayers.add(new Player("Sara", "Playing", "Invite"));
-        allPlayers.add(new Player("Mona", "Online", "Accept"));
-        allPlayers.add(new Player("Ali", "Online", "Accept"));
-        allPlayers.add(new Player("Mariam", "Online", "Decline"));
-        allPlayers.add(new Player("Ahmed", "Online", "Invite"));
-        allPlayers.add(new Player("Sara", "Playing", "Invite"));
-        allPlayers.add(new Player("Mona", "Online", "Invite"));
-        allPlayers.add(new Player("Ali", "Online", "Accept"));
-        allPlayers.add(new Player("Ahmed", "Online", "Invite"));
-        allPlayers.add(new Player("Sara", "Playing", "Invite"));
-        allPlayers.add(new Player("Mona", "Online", "Invite"));
-        allPlayers.add(new Player("Ali", "Online", "Decline"));
-        allPlayers.add(new Player("Ahmed", "Online", "Invite"));
-        allPlayers.add(new Player("Sara", "Playing", "Invite"));
-        allPlayers.add(new Player("Mona", "Online", "Accept"));
-        allPlayers.add(new Player("Ali", "Online", "Accept"));
-        allPlayers.add(new Player("Mariam", "Online", "Decline"));
-        allPlayers.add(new Player("Ahmed", "Online", "Invite"));
-        allPlayers.add(new Player("Sara", "Playing", "Invite"));
-        allPlayers.add(new Player("Mona", "Online", "Invite"));
-        allPlayers.add(new Player("Ali", "Online", "Accept"));
-        allPlayers.add(new Player("Ahmed", "Online", "Invite"));
-        allPlayers.add(new Player("Sara", "Playing", "Invite"));
-        allPlayers.add(new Player("Mona", "Online", "Invite"));
-        allPlayers.add(new Player("Ali", "Online", "Decline"));
+        client = Client.getInstance();
+        cp = ClientProtocol.getInstance();
+        onlinePlayers = cp.getPlayers();
+        updateListView(new ArrayList<>(onlinePlayers));
+    }
 
-        updateListView(allPlayers);
-
-        searchBar.textProperty().addListener((observable, oldValue, newValue) -> {filterList(newValue);});
-
-        playersListView.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal) -> {
-            if (newVal == null) {
-                return;
-            }
-            String actionType = (String) newVal.getUserData();
-            System.out.println(actionType);
-            if ("Invite".equals(actionType)) {
-                try {
-                    App.setRoot("fxml/invetation");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
+    @FXML
+    private void onSearch(ActionEvent event) {
+        String searchText = searchBar.getText();
+        filterList(searchText);
     }
 
     private void filterList(String searchText) {
-        List<Player> filteredList = new ArrayList<>();
-        for (Player p : allPlayers) {
-            if (p.name.contains(searchText)) {
+        List<PlayerDTO> filteredList = new ArrayList<>();
+        for (PlayerDTO p : onlinePlayers) {
+            if (p.getUserName().contains(searchText)) {
                 filteredList.add(p);
             }
         }
         updateListView(filteredList);
     }
 
-    private void updateListView(List<Player> players) {
+    private void updateListView(List<PlayerDTO> players) {
         playersListView.getItems().clear();
         if (players.isEmpty()) {
             playersListView.setVisible(false);
@@ -107,14 +67,13 @@ public class OnlinePlayersController implements Initializable {
         } else {
             playersListView.setVisible(true);
             noPlayersLabel.setVisible(false);
-
-            for (Player p : players) {
-                addPlayerToList(p.name, p.status, p.actionType);
+            for (PlayerDTO p : players) {
+                addPlayerToList(p.getUserName(), p.getStatus().name());
             }
         }
     }
 
-    private void addPlayerToList(String name, String status, String type) {
+    private void addPlayerToList(String name, String status) {
         HBox row = new HBox(10);
         row.setAlignment(Pos.CENTER_LEFT);
 
@@ -129,58 +88,35 @@ public class OnlinePlayersController implements Initializable {
         }
 
         details.getChildren().addAll(nameLabel, statusLabel);
-
         Region spacer = new Region();
         HBox.setHgrow(spacer, Priority.ALWAYS);
-
         row.getChildren().addAll(details, spacer);
 
-        if (type.equals("Invite")) {
-            Button inviteBtn = new Button("Invite");
-            if (status.equals("Playing")) {
-                inviteBtn.setStyle("-fx-background-color: #ADADAD; -fx-text-fill: white; -fx-background-radius: 15;");
-            } else {
-                inviteBtn.setStyle("-fx-background-color: #2196F3; -fx-text-fill: white; -fx-background-radius: 15;");
-            }
-            inviteBtn.setOnAction(e -> System.out.println("Inviting " + name));
-            row.getChildren().add(inviteBtn);
-        } else if (type.equals("Accept")) {
-            Button acceptBtn = new Button("Accept");
-            acceptBtn.setStyle("-fx-background-color: #00C096; -fx-text-fill: white; -fx-background-radius: 15;");
-            row.getChildren().add(acceptBtn);
+        Button inviteBtn = new Button("Invite");
 
-        } else if (type.equals("Decline")) {
-            Button declineBtn = new Button("Decline");
-            declineBtn.setStyle("-fx-background-color: #FF827E; -fx-text-fill: white; -fx-background-radius: 15;");
-            row.getChildren().add(declineBtn);
-
+        if (status.equals("Playing")) {
+            inviteBtn.setStyle("-fx-background-color: #ADADAD; -fx-text-fill: white; -fx-background-radius: 15;");
+        } else {
+            inviteBtn.setStyle("-fx-background-color: #2196F3; -fx-text-fill: white; -fx-background-radius: 15;");
         }
-         row.setUserData(type);
 
+        inviteBtn.setOnAction(e -> {
+            if (inviteBtn.getText().equals("Invite")) {
+                inviteBtn.setText("Sent");
+                inviteBtn.setDisable(true);
+                client.sendMessage("SEND_INVITE:" + name);
+            }
+        });
+
+        if (!(name.equals(Client.getInstance().getPlayer().getUserName()))) {
+            row.getChildren().add(inviteBtn);
+        }
+        
         playersListView.getItems().add(row);
     }
 
     @FXML
-    private void onSearch(ActionEvent event) {
-    }
-
-    @FXML
-    private void goBackOnClick(ActionEvent event) throws IOException {
-        App.setRoot("fxml/menu");
-    }
-
-}
-
-//TODO -> convert it to POJO
-class Player {
-
-    String name;
-    String status;
-    String actionType;
-
-    public Player(String name, String status, String actionType) {
-        this.name = name;
-        this.status = status;
-        this.actionType = actionType;
+    private void goBackOnClick(ActionEvent event) {
+        Functions.naviagteTo("fxml/menu");
     }
 }
